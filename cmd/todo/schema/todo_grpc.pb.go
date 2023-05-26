fff52869425365024e63eeabc777d04d2a17a2d5
Alex Dawson <alex.dawson@thirdlight.com>
2023-05-26 23:24:18 +0100

    Update Todo service

--- cmd/todo/schema/todo_grpc.pb.go
+++ cmd/todo/schema/todo_grpc.pb.go
@@ -20,13 +20,15 @@ const _ = grpc.SupportPackageIsVersion7
 
 const (
 	Todo_CreateTodo_FullMethodName = "/schema.Todo/CreateTodo"
+	Todo_GetTodo_FullMethodName    = "/schema.Todo/GetTodo"
 )
 
 // TodoClient is the client API for Todo service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type TodoClient interface {
-	CreateTodo(ctx context.Context, in *CreateTodoRequest, opts ...grpc.CallOption) (*CreateTodoResponse, error)
+	CreateTodo(ctx context.Context, in *CreateTodoRequest, opts ...grpc.CallOption) (*GetTodoResponse, error)
+	GetTodo(ctx context.Context, in *GetTodoRequest, opts ...grpc.CallOption) (*GetTodoResponse, error)
 }
 
 type todoClient struct {
@@ -37,9 +39,18 @@ func NewTodoClient(cc grpc.ClientConnInterface) TodoClient {
 	return &todoClient{cc}
 }
 
-func (c *todoClient) CreateTodo(ctx context.Context, in *CreateTodoRequest, opts ...grpc.CallOption) (*CreateTodoResponse, error) {
-	out := new(CreateTodoResponse)
+func (c *todoClient) CreateTodo(ctx context.Context, in *CreateTodoRequest, opts ...grpc.CallOption) (*GetTodoResponse, error) {
+	out := new(GetTodoResponse)
 	err := c.cc.Invoke(ctx, Todo_CreateTodo_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *todoClient) GetTodo(ctx context.Context, in *GetTodoRequest, opts ...grpc.CallOption) (*GetTodoResponse, error) {
+	out := new(GetTodoResponse)
+	err := c.cc.Invoke(ctx, Todo_GetTodo_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -50,7 +61,8 @@ func (c *todoClient) CreateTodo(ctx context.Context, in *CreateTodoRequest, opts
 // All implementations must embed UnimplementedTodoServer
 // for forward compatibility
 type TodoServer interface {
-	CreateTodo(context.Context, *CreateTodoRequest) (*CreateTodoResponse, error)
+	CreateTodo(context.Context, *CreateTodoRequest) (*GetTodoResponse, error)
+	GetTodo(context.Context, *GetTodoRequest) (*GetTodoResponse, error)
 	mustEmbedUnimplementedTodoServer()
 }
 
@@ -58,8 +70,11 @@ type TodoServer interface {
 type UnimplementedTodoServer struct {
 }
 
-func (UnimplementedTodoServer) CreateTodo(context.Context, *CreateTodoRequest) (*CreateTodoResponse, error) {
+func (UnimplementedTodoServer) CreateTodo(context.Context, *CreateTodoRequest) (*GetTodoResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateTodo not implemented")
+}
+func (UnimplementedTodoServer) GetTodo(context.Context, *GetTodoRequest) (*GetTodoResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetTodo not implemented")
 }
 func (UnimplementedTodoServer) mustEmbedUnimplementedTodoServer() {}
 
@@ -92,6 +107,24 @@ func _Todo_CreateTodo_Handler(srv interface{}, ctx context.Context, dec func(int
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Todo_GetTodo_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetTodoRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TodoServer).GetTodo(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Todo_GetTodo_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TodoServer).GetTodo(ctx, req.(*GetTodoRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Todo_ServiceDesc is the grpc.ServiceDesc for Todo service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -102,6 +135,10 @@ var Todo_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "CreateTodo",
 			Handler:    _Todo_CreateTodo_Handler,
+		},
+		{
+			MethodName: "GetTodo",
+			Handler:    _Todo_GetTodo_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
